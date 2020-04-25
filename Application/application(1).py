006a1d1833826891f68e93e70a4d3cb868f30fce
Marcovin <mvtquinones@gmail.com>
2020-04-25 21:32:58 +0800

    Update on Application functionalities

---
+++ Application/application(1).py
@@ -0,0 +1,174 @@
+import win32com.client as wincl
+import sqlite3 
+import speech_recognition as sr
+import os
+import webbrowser
+import multiprocessing
+import time
+new = 2
+googleUrl="http://google.com/?#q="
+youtubeUrl="http://youtube.com/results?search_query="
+
+def talk(speech):
+    speak = wincl.Dispatch("SAPI.SpVoice")
+    speak.speak(speech)
+    
+def recognize():
+    r = sr.Recognizer()
+    try:
+        with sr.Microphone() as source:
+            print("say something")
+            r.adjust_for_ambient_noise(source)
+            audio = r.listen(source,timeout=10, phrase_time_limit=6)
+        
+        text = r.recognize_google(audio)
+        print("You said : {}".format(text))
+    
+    except Exception as e:
+        print(e)
+        talk(e)
+    
+    return(text)
+    
+def commands():
+    text = recognize()
+    if text[0:20] == "jarvis search google":
+        p1=multiprocessing.Process(target=talk, args=('Okay, searching google for {}'.format(text[21:]), ))
+        p2=multiprocessing.Process(target=webbrowser.open, args=(googleUrl+text[21:], 'new=new'))
+        p1.start()
+        p2.start()
+        p1.join()
+        p2.join()
+
+    if text[0:21] == "jarvis search youtube":
+        p1=multiprocessing.Process(target=talk, args=('Okay, searching youtube for {}'.format(text[22:]), ))
+        p2=multiprocessing.Process(target=webbrowser.open, args=(youtubeUrl+text[22:], 'new=new'))
+        p1.start()
+        p2.start()
+        p1.join()
+        p2.join()
+#        add_history("search", "youtube")
+        
+    if text == "jarvis open notepad":
+        p1=multiprocessing.Process(target=talk, args=('Opening notepad', ))
+        p2=multiprocessing.Process(target=os.system, args=('start notepad.exe', ))
+        p1.start()
+        p2.start()
+        p1.join()
+        p2.join()
+
+    if text == "jarvis open chrome":
+        p1=multiprocessing.Process(target=talk, args=('Opening chrome', ))
+        p2=multiprocessing.Process(target=os.system, args=('start chrome.exe', ))
+        p1.start()
+        p2.start()
+        p1.join()
+        p2.join()
+
+    if text == "jarvis open word":
+        p1=multiprocessing.Process(target=talk, args=('Opening word', ))
+        p2=multiprocessing.Process(target=os.system, args=('start WINWORD.exe', ))
+        p1.start()
+        p2.start()
+        p1.join()
+        p2.join()
+    
+    if text == "jarvis open excel":
+        p1=multiprocessing.Process(target=talk, args=('Opening excel', ))
+        p2=multiprocessing.Process(target=os.system, args=('start EXCEL.exe', ))
+        p1.start()
+        p2.start()
+        p1.join()
+        p2.join()
+    
+    if text == "jarvis open powerpoint":
+        p1=multiprocessing.Process(target=talk, args=('Opening powerpoint', ))
+        p2=multiprocessing.Process(target=os.system, args=('start POWERPNT.exe', ))
+        p1.start()
+        p2.start()
+        p1.join()
+        p2.join()
+
+    if text == "jarvis open onenote":
+        p1=multiprocessing.Process(target=talk, args=('Opening onenote', ))
+        p2=multiprocessing.Process(target=os.system, args=('start ONENOTE.exe', ))
+        p1.start()
+        p2.start()
+        p1.join()
+        p2.join()
+
+    if text == "jarvis open outlook":
+        p1=multiprocessing.Process(target=talk, args=('Opening outlook', ))
+        p2=multiprocessing.Process(target=os.system, args=('start OUTLOOK.exe', ))
+        p1.start()
+        p2.start()
+        p1.join()
+        p2.join()
+
+
+    if text == "jarvis open matlab":
+        p1=multiprocessing.Process(target=talk, args=('Opening matlab', ))
+        p2=multiprocessing.Process(target=os.system, args=('start matlab.exe', ))
+        p1.start()
+        p2.start()
+        p1.join()
+        p2.join()
+
+    if text == "jarvis open fileexplorer" or "jarvis open explorer":
+        p1=multiprocessing.Process(target=talk, args=('Opening fileexplorer', ))
+        p2=multiprocessing.Process(target=os.system, args=('start explorer.exe', ))
+        p1.start()
+        p2.start()
+        p1.join()
+        p2.join()
+
+    if text == "jarvis open prompt":
+        p1=multiprocessing.Process(target=talk, args=('Opening commandprompt', ))
+        p2=multiprocessing.Process(target=os.system, args=('start cmd.exe', ))
+        p1.start()
+        p2.start()
+        p1.join()
+        p2.join()
+
+    if text == "jarvis lock computer":
+        p1=multiprocessing.Process(target=talk, args=('Locking Computer', ))
+        p2=multiprocessing.Process(target=os.system, args=('rundll32.exe user32.dll,LockWorkStation', ))
+        p1.start()
+        p2.start()
+        p1.join()
+        p2.join()
+
+    if text == "jarvis sleep":
+        p1=multiprocessing.Process(target=talk, args=('Sleeping...', ))
+        p2=multiprocessing.Process(target=os.system, args=('RUNDLL32.EXE powrprof.dll,SetSuspendState 0,1,0', ))
+        p1.start()
+        p2.start()
+        p1.join()
+        p2.join()
+
+         
+    if text == "jarvis shutdown computer" or "jarvis turn off pc":
+        p1=multiprocessing.Process(target=talk, args=('Shutting down computer in 20 seconds', ))
+        p2=multiprocessing.Process(target=os.system, args=('shutdown /s /t 20', ))
+        p1.start()
+        p2.start()
+        p1.join()
+        p2.join()
+ 
+    if text == "jarvis restart computer" or "jarvis restart pc":
+        p1=multiprocessing.Process(target=talk, args=('Restarting computer in 20 seconds', ))
+        p2=multiprocessing.Process(target=os.system, args=('shutdown /r /t 20', ))
+        p1.start()
+        p2.start()
+        p1.join()
+        p2.join()
+
+    if text == "jarvis display history":
+        talk("Displaying command history.")
+        action = "SELECT * FROM history"
+#        show_history()
+                
+        
+    
+
+    
