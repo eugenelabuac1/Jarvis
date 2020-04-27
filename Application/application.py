34b6a7c98001f8e2c54e6d22f5bf1f684fcf92b9
jsgonzales1011 <61183613+jsgonzales1011@users.noreply.github.com>
2020-04-27 18:48:38 +0800

    Added additional commands

--- Application/application.py
+++ Application/application.py
@@ -42,7 +42,8 @@ def commands():
         p2.start()
         p1.join()
         p2.join()
-
+        add_history('search',text[14:])
+        
     if text[0:21] == "jarvis search youtube":
         p1=multiprocessing.Process(target=talk, args=('Okay, searching youtube for {}'.format(text[22:]), ))
         p2=multiprocessing.Process(target=webbrowser.open, args=(youtubeUrl+text[22:], 'new=new'))
@@ -50,7 +51,7 @@ def commands():
         p2.start()
         p1.join()
         p2.join()
-#        add_history("search", "youtube")
+        add_history("search", "youtube")
         
     if text == "jarvis open notepad":
         p1=multiprocessing.Process(target=talk, args=('Opening notepad', ))
@@ -59,7 +60,8 @@ def commands():
         p2.start()
         p1.join()
         p2.join()
-
+        add_history('open', 'notepad')
+        
     if text == "jarvis open chrome":
         p1=multiprocessing.Process(target=talk, args=('Opening chrome', ))
         p2=multiprocessing.Process(target=os.system, args=('start chrome.exe', ))
@@ -67,7 +69,8 @@ def commands():
         p2.start()
         p1.join()
         p2.join()
-
+        add_history('open', 'chrome')
+        
     if text == "jarvis open word":
         p1=multiprocessing.Process(target=talk, args=('Opening word', ))
         p2=multiprocessing.Process(target=os.system, args=('start WINWORD.exe', ))
@@ -75,6 +78,7 @@ def commands():
         p2.start()
         p1.join()
         p2.join()
+        add_history('open', 'word')
     
     if text == "jarvis open excel":
         p1=multiprocessing.Process(target=talk, args=('Opening excel', ))
@@ -83,6 +87,7 @@ def commands():
         p2.start()
         p1.join()
         p2.join()
+        add_history('open', 'excel')
     
     if text == "jarvis open powerpoint":
         p1=multiprocessing.Process(target=talk, args=('Opening powerpoint', ))
@@ -91,7 +96,8 @@ def commands():
         p2.start()
         p1.join()
         p2.join()
-         
+        add_history('open', 'powerpoint')
+                
     if text == "jarvis shutdown computer":
         p1=multiprocessing.Process(target=talk, args=('Shutting down computer in 20 seconds', ))
         p2=multiprocessing.Process(target=os.system, args=('shutdown /s /t 20', ))
@@ -99,7 +105,8 @@ def commands():
         p2.start()
         p1.join()
         p2.join()
- 
+        add_history('shutdown', '')
+        
     if text == "jarvis restart computer":
         p1=multiprocessing.Process(target=talk, args=('Restarting computer in 20 seconds', ))
         p2=multiprocessing.Process(target=os.system, args=('shutdown /r /t 20', ))
@@ -107,13 +114,43 @@ def commands():
         p2.start()
         p1.join()
         p2.join()
+        add_history('restart', '')
+
+#
 
     if text == "jarvis display history":
         talk("Displaying command history.")
         action = "SELECT * FROM history"
-#        show_history()
-                
+        #hist.Show_ThirdWindow(action)
+        show_history()
         
+    if text == "jarvis clear history":
+        talk("Clearing command  history.")
+        action = "SELECT * FROM history"
+        #hist.Show_ThirdWindow(action)
+        clear_history()
+        show_history()
+        
+    if text == "jarvis display open":
+        talk("Displaying history wherein action type is open.")
+        action = "SELECT * FROM history WHERE action_type = 'open' "
+        #hist.Show_ThirdWindow(action)
+        read_from_db_open()
     
-
+    if text == "jarvis display search":
+        talk("Displaying history wherein action type is search.")
+        action = "SELECT * FROM history WHERE action_type = 'search' "
+        #hist.Show_ThirdWindow(action)
+        read_from_db_search()
+        
+    if text == "jarvis display restart":
+        talk("Displaying history wherein action type is restart.")
+        action = "SELECT * FROM history WHERE action_type = 'restart' "
+        #hist.Show_ThirdWindow(action)
+        read_from_db_restart()
     
+    if text == "jarvis display shutdown":
+        talk("Displaying history wherein action type is shutdown.")
+        action = "SELECT * FROM history WHERE action_type = 'shutdown' "
+        #hist.Show_ThirdWindow(action)
+        read_from_db_shutdown()
