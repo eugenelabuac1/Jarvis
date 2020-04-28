753708e30191cc80a490e04a3faeffc5c0fe8dcb
jsgonzales <jersgo1011@gmail.com>
2020-04-28 20:54:27 +0800

    added history windows

--- application.py
+++ application.py
@@ -1,13 +1,14 @@
 import win32com.client as wincl
-import sqlite3 
 import speech_recognition as sr
 import os
 import webbrowser
 import multiprocessing
-import time
 import sys
 sys.path.append('../')
 from Database.db import *
+from GUI.hist import *
+
+
 new = 2
 googleUrl="http://google.com/?#q="
 youtubeUrl="http://youtube.com/results?search_query="
@@ -16,25 +17,26 @@ def talk(speech):
     speak = wincl.Dispatch("SAPI.SpVoice")
     speak.speak(speech)
     
-def recognize():
-    r = sr.Recognizer()
-    try:
-        with sr.Microphone() as source:
-            print("say something")
-            r.adjust_for_ambient_noise(source)
-            audio = r.listen(source,timeout=10, phrase_time_limit=6)
+def recognize(text):
+    if '{}'.format(text) == "":
+        r = sr.Recognizer()
+        try:
+            with sr.Microphone() as source:
+                print("say something")
+                r.adjust_for_ambient_noise(source)
+                audio = r.listen(source,timeout=10, phrase_time_limit=6)
         
-        text = r.recognize_google(audio)
-        print("You said : {}".format(text))
+            text = r.recognize_google(audio)
+            print("You said : {}".format(text))
     
-    except Exception as e:
-        print(e)
-        talk(e)
+        except Exception as e:
+            print(e)
+            talk(e) 
+        commands(text)
+    else:
+        commands('{}'.format(text))
     
-    return(text)
-    
-def commands():
-    text = recognize()
+def commands(text):
     if text[0:20] == "jarvis search google":
         p1=multiprocessing.Process(target=talk, args=('Okay, searching google for {}'.format(text[21:]), ))
         p2=multiprocessing.Process(target=webbrowser.open, args=(googleUrl+text[21:], 'new=new'))
@@ -119,38 +121,45 @@ def commands():
 #
 
     if text == "jarvis display history":
-        talk("Displaying command history.")
         action = "SELECT * FROM history"
-        #hist.Show_ThirdWindow(action)
+        talk("Displaying command history")
+        passAction(action)
         show_history()
         
     if text == "jarvis clear history":
         talk("Clearing command  history.")
         action = "SELECT * FROM history"
-        #hist.Show_ThirdWindow(action)
         clear_history()
+        passAction(action)
         show_history()
         
     if text == "jarvis display open":
         talk("Displaying history wherein action type is open.")
         action = "SELECT * FROM history WHERE action_type = 'open' "
-        #hist.Show_ThirdWindow(action)
+        passAction(action)
         read_from_db_open()
     
     if text == "jarvis display search":
         talk("Displaying history wherein action type is search.")
         action = "SELECT * FROM history WHERE action_type = 'search' "
-        #hist.Show_ThirdWindow(action)
+        passAction(action)
         read_from_db_search()
         
     if text == "jarvis display restart":
         talk("Displaying history wherein action type is restart.")
         action = "SELECT * FROM history WHERE action_type = 'restart' "
-        #hist.Show_ThirdWindow(action)
+        passAction(action)
         read_from_db_restart()
     
     if text == "jarvis display shutdown":
         talk("Displaying history wherein action type is shutdown.")
         action = "SELECT * FROM history WHERE action_type = 'shutdown' "
-        #hist.Show_ThirdWindow(action)
+        passAction(action)
         read_from_db_shutdown()
+        
+
+
+def passAction(action):
+    gui = ShowWindow3
+    gui.action = action
+    gui.Show_ThirdWindow(gui,action)
